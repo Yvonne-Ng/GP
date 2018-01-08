fb95e79ffab9d69a3fe3ecb9d2152f5cb8752a4d
Yvonne Ng <yvonne.ng@cern.ch>
2018-01-08 04:50:16 -0800

    broken reconstruction function

--- Libplotmeghan.py
+++ Libplotmeghan.py
@@ -83,6 +83,8 @@ class Mean():
 
 def get_kernel(Amp, decay, length, power, sub): # getting the typical background kernel
     return Amp * MyDijetKernelSimp(a = decay, b = length, c = power, d = sub)
+def get_kernel_SigOnly(A, mass, tau):
+    return A* LocalGaussianKernel(mass, tau)
 
 def get_kernel_Xtreme(Amp, decay, length, power, sub, A, mass, tau): # getting the signal+bkgnd kernel
     #kernelsig = Amp * ExpSquaredCenteredKernel(m=mass, t=tau)
@@ -121,13 +123,14 @@ def fit_gp_minuit(num, lnprob):
     min_likelihood = np.inf
     best_fit_params = (0, 0, 0, 0, 0,0,0,0)
     guesses = {
-        'amp': 5701461179.0,
+        'amp': 6946279097.0,
         #'p0': 0.23,
         #'p1': 0.46,
         #'p2': 0.89
-        'p0': 0.21,
-        'p1': 28.38,
-        'p2': -2.538
+        #gotta change this to the fit function value!
+        'p0': 6.00,
+        'p1': 19.43,
+        'p2': -2.307
     }
     def bound(par, neg=False):
         if neg:
@@ -163,7 +166,7 @@ def fit_gp_minuit(num, lnprob):
                    limit_Amp = bound('amp'),
                    limit_decay = (0.01, 1000),
                    limit_length = (1, 1e8),
-                   limit_power = (0.001, 1000),
+                   limit_power = (0.0001, 100),
                    limit_sub = (0.001, 1e6),
                    #limit_p0 = bound('p0', neg=false),
                    #limit_p1 = bound('p1', neg=true),
@@ -172,13 +175,122 @@ def fit_gp_minuit(num, lnprob):
                    limit_p1 = (-100,100),
                    limit_p2 = (-100,100))
         m.migrad()
-        #print("trial #", i)
-        #print ("likelihood: ",m.fval)
-        #print("args: ", m.args)
+        print("trial #", i)
+        print ("likelihood: ",m.fval)
+        print("args: ", m.args)
         if m.fval < min_likelihood and m.fval != 0.0:
             min_likelihood = m.fval
             best_fit_params = m.values
     print("GP bkgnd kernel")
+    print("min ll", min_likelihood)
+    print(f'best fit params {best_fit_params}')
+    return min_likelihood, best_fit_params
+
+class logLike_minuit_sig:
+    def __init__(self, x, y, xerr):
+        self.x = x
+        self.y = y
+        self.xerr = xerr
+    def __call__(self, Amp, decay, length, power, sub, p0, p1, p2, A=0, mass=0, tau=0):
+        #kernel = get_kernel(Amp, decay, length, power, sub)
+        kernel1 = Amp * MyDijetKernelSimp(a=decay, b=length, c=power, d=sub)
+        kernel2 = A * LocalGaussianKernel(mass, tau)
+        kernel = kernel1 + kernel2
+        #kernel=kernel1
+        mean = Mean((p0,p1,p2))
+        gp = george.GP(kernel, mean=mean, fit_mean = True)
+        try:
+            gp.compute(self.x, np.sqrt(self.y))
+            return -gp.lnlikelihood(self.y, self.xerr)
+        except:
+            return np.inf
+#####################3#
+#    def __call__(self, Amp, decay, length, power, sub, p0, p1, p2, A=0, mass=0, tau=0):
+#        #Amp, decay, length, power, sub, p0, p1, p2 = fixedHyperparams
+#        kernel1 = Amp * MyDijetKernelSimp(a=decay, b=length, c=power, d=sub)
+#        kernel2 = A * LocalGaussianKernel(mass, tau)
+#        kernel = kernel1 #+ kernel2
+#        #kernel=kernel1
+#        gp = george.GP(kernel)
+#        try:
+#            gp.compute(self.x, np.sqrt(self.y))
+#            return -gp.lnlikelihood(self.y, self.xerr)
+#        except:
+#            return np.inf
+###
+def fit_gpSig_minuit(num, lnprob, initParams):
+
+    min_likelihood = np.inf
+    best_fit_params = (0, 0, 0, 0, 0,0,0,0)
+    guesses = {
+        'amp': 6946279097.0,
+        #'p0': 0.23,
+        #'p1': 0.46,
+        #'p2': 0.89
+        #gotta change this to the fit function value!
+        'p0': 6.00,
+        'p1': 19.43,
+        'p2': -2.307
+    }
+    def bound(par, neg=False):
+        if neg:
+            return (-2.0*guesses[par], 2.0*guesses[par])
+        return (guesses[par] * 0.5, guesses[par] * 2.0)
+
+    for i in range(num):
+        iamp = initParams[0]#i*1.2*np.random.random()# * 2*guesses['amp']
+        idecay = initParams[1]#*1.2*np.random.random()# * 0.64
+        ilength = initParams[2]#*1.2*np.random.random()# * 5e5
+        ipower = initParams[3]#*1.2*np.random.random()# * 1.0
+        isub = initParams[4]#*1.2*np.random.random()# * 1.0
+        ip0 =initParams[5] #*1.2*np.random.random() #guesses['p0']
+        ip1 = initParams[6]#*1.2*np.random.random() #guesses['p1']
+        ip2 = initParams[7]#*1.2*np.random.random() #guesses['p2']
+        iA = (2500000 *np.random.random())
+        itau = (200 * np.random.random())
+        iMass = (500 * np.random.random())
+
+        m = Minuit(lnprob, throw_nan = True, pedantic = True,
+                   print_level = 0, errordef = 0.5,
+                   Amp = iamp,decay = idecay, length = ilength,power = ipower,sub = isub,
+                   p0 = ip0, p1 = ip1, p2 = ip2,
+                   A=iA, tau=itau, mass=iMass,
+                   error_Amp = 1e1,
+                   error_decay = 1e1,
+                   error_length = 1e1,
+                   error_power = 1e1,
+                   error_sub = 1e1,
+                   error_p0 = 1e-2, error_p1 = 1e-2, error_p2 = 1e-2,
+                   error_A = 1e-2, error_tau = 1e-2, error_mass = 1e-2,
+                   limit_Amp = (initParams[0], initParams[0]*1.001),#bound('amp'),
+                   limit_decay = (initParams[1], initParams[1]*1.001),
+                   limit_length = (initParams[2], initParams[2]*1.001),
+                   limit_power = (initParams[3], initParams[3]*1.001),
+                   limit_sub = (initParams[4], initParams[4]*1.001),
+                   #limit_decay = (0.01, 1000),
+                   #limit_length = (1, 1e8),
+                   #limit_power = (0.0001, 100),
+                   #limit_sub = (0.001, 1e6),
+                   #limit_p0 = bound('p0', neg=false),
+                   #limit_p1 = bound('p1', neg=true),
+                   #limit_p2 = bound('p2', neg=true))
+                   #limit_p0 = (-100,1000000),
+                   #limit_p1 = (-100,100),
+                   #limit_p2 = (-100,100),
+                   limit_p0 = (initParams[5],initParams[5]*1.001),
+                   limit_p1 = (initParams[6], initParams[6]*1.001),
+                   limit_p2 = (initParams[7],initParams[7]*0.9999),
+                   limit_A = (500, 2500000),
+                   limit_tau = (50, 300),
+                   limit_mass=(400, 550))
+        m.migrad()
+        print("trial #", i)
+        print ("likelihood: ",m.fval)
+        print("args: ", m.args)
+        if( m.fval < min_likelihood )and( m.fval != 0.0):
+            min_likelihood = m.fval
+            best_fit_params = m.values
+    print("GP sig kernel from bkgnd")
     print("min ll", min_likelihood)
     print(f'best fit params {best_fit_params}')
     return min_likelihood, best_fit_params
@@ -190,11 +302,11 @@ class logLike_gp_fitgpsig:
         self.y = y
         self.xerr = xerr
 
-    def __call__(self, Amp, decay, length, power, sub, p0, p1, p2, A, mass, tau):
+    def __call__(self, Amp, decay, length, power, sub, p0, p1, p2, A=0, mass=0, tau=0):
         #Amp, decay, length, power, sub, p0, p1, p2 = fixedHyperparams
         kernel1 = Amp * MyDijetKernelSimp(a=decay, b=length, c=power, d=sub)
         kernel2 = A * LocalGaussianKernel(mass, tau)
-        kernel = kernel1 + kernel2
+        kernel = kernel1 #+ kernel2
         #kernel=kernel1
         gp = george.GP(kernel)
         try:
@@ -202,31 +314,68 @@ class logLike_gp_fitgpsig:
             return -gp.lnlikelihood(self.y, self.xerr)
         except:
             return np.inf
-def fit_gp_fitgpsig_minuit(num, lnprob, Print=True):
+
+def fit_gp_fitgpsig_minuit(num, lnprob, initParams,Print=True):
     min_likelihood = np.inf
     best_fit_params = (0, 0, 0, 0, 0,0,0,0)
     for i in range(num):
-        iAmp= np.random.random() *5701461179.0
-        iDecay = np.random.random() *0.64
-        ilength = np.random.random() *5e5
-        ipower =np.random.random() * 1.0
-        isub= np.random.random() * 1.0
+        #iAmp= np.random.random() *5701461179.0
+        #iDecay = np.random.random() *0.64
+        #ilength = np.random.random() *5e5
+        #ipower =np.random.random() * 1.0
+        #isub= np.random.random() * 1.0
+        ##ip0=np.random.random() * 0.23
+        ##ip1= np.random.random() *0.46
+        ##ip2=np.random.random() *0.89
         #ip0=np.random.random() * 0.23
-        #ip1= np.random.random() *0.46
-        #ip2=np.random.random() *0.89
-        ip0=np.random.random() * 0.23
-        ip1= np.random.random() *28.38
-        ip2=np.random.random() *-2.538
-        iA = 0
+        #ip1= np.random.random() *28.38
+        #ip2=np.random.random() *-2.538
+        #iA = 0
+        #imass=np.random.random() *500
+        #itau= np.random.random() *100000000
+#signal 
+        #iAmp= np.random.random() *2880395166.7137227
+        #iDecay =np.random.random() * 24.80421529029139
+        #ilength = np.random.random() *1.0000000666133808
+        #ipower = np.random.random() *0.001
+        #isub= np.random.random() *632.2987
+        #ip0=np.random.random() *7.099
+        #ip1= np.random.random() *19.85
+        #ip2=np.random.random() *-2.24
+        #iA = (np.random.random() *2)*0.000001
+        #imass=np.random.random() *np.random.random() *500
+        #itau= np.random.random() *np.random.random() *100000000
+
+        #iAmp=np.random.random() *2880395166.7137227 
+        #iDecay =np.random.random() *24.80
+        #ilength =np.random.random() * 1.00
+        #ipower =np.random.random() * 0.0010
+        #isub= np.random.random() *632.298
+        #ip0=np.random.random() *7.09
+        #ip1=np.random.random() * 19.83
+        #ip2=np.random.random() *-2.24
+        #iA =np.random.random() *0#(np.random.random() *2)*0.000001
+        #imass=np.random.random() *500
+        #itau= np.random.random() *100000000
+
+        iAmp=np.random.random() * initParams[0]
+        iDecay =np.random.random() * initParams[1]
+        ilength =np.random.random() *initParams[2]
+        ipower =np.random.random() * initParams[3]
+        isub= np.random.random() *initParams[4]
+        ip0=np.random.random() *initParams[5]
+        ip1=np.random.random() * initParams[6]
+        ip2=np.random.random() * initParams[7]
+        iA =np.random.random() *0#(np.random.random() *2)*0.000001
         imass=np.random.random() *500
         itau= np.random.random() *100000000
 
-        m = Minuit(lnprob, throw_nan=True, pedantic=True, print_level=0, errordef=0.5, Amp=iAmp, decay=iDecay, length=ilength, power=ipower, sub=isub, p0=ip0, p1=ip1, p2=ip2, A=iA, mass=imass, tau=itau, error_Amp=1e1, error_decay=1e1, error_length=1e1, error_power=1e1, error_sub=1e1, error_p0=1e-2, error_p1=1e-2, error_p2=1e-2, error_A=1., error_mass=1., error_tau=1., limit_Amp=(-100000,100000000000), limit_decay=(0.01, 1000), limit_length=(1, 1e8), limit_power=(0.001, 1000), limit_sub=(0.001, 1e6), limit_p0=(-100,1000000.), limit_p1=(-100., 100.), limit_p2=(-100., 100), limit_A=(-100,100), limit_mass=(450, 550), limit_tau=(-500, 500))
+        m = Minuit(lnprob, throw_nan=True, pedantic=True, print_level=0, errordef=0.5, Amp=iAmp, decay=iDecay, length=ilength, power=ipower, sub=isub, p0=ip0, p1=ip1, p2=ip2, A=iA, mass=imass, tau=itau, error_Amp=1e1, error_decay=1e1, error_length=1e1, error_power=1e1, error_sub=1e1, error_p0=1e-2, error_p1=1e-2, error_p2=1e-2, error_A=1., error_mass=1., error_tau=1., limit_Amp=(-100000,100000000000), limit_decay=(0.01, 1000), limit_length=(1, 1e8), limit_power=(0.001, 1000), limit_sub=(0.001, 1e6), limit_p0=(-100,1000000.), limit_p1=(-100., 100.), limit_p2=(-100., 100), limit_A=(-0.1,0.1), limit_mass=(450, 550), limit_tau=(-500, 500))
 
         m.migrad()
-        #print("trial #", i)
-        #print ("likelihood: ",m.fval)
-        #print("args: ", m.args)
+        print("trial #", i)
+        print ("likelihood: ",m.fval)
+        print("args: ", m.args)
         if m.fval < min_likelihood and m.fval != 0.0:
             min_likelihood = m.fval
             best_fit_params = m.values
@@ -267,6 +416,18 @@ class logLike_3ff:
         except:
             return np.inf
 
+def logLike_3ffOff(x, y, xFit, yFit, xe):
+    logL = 0
+    for ibin in range(len(y)):
+        data = y[ibin]
+        bkg = yFit[ibin]
+        logL += -simpleLogPoisson(data, bkg)
+    try:
+        logL
+        return logL
+    except:
+        return np.inf
+
 def fit_3ff(num,lnprob, Print=True):
     minLLH = np.inf
     best_fit_params = (0., 0., 0.)
@@ -275,9 +436,10 @@ def fit_3ff(num,lnprob, Print=True):
         #init1 = np.random.random() * 0.46
         #init2 = np.random.random() * 0.8901
 
-        init0 =  np.random.random() *0.21
-        init1 = np.random.random() * 28.38
-        init2 = np.random.random() * -2.53
+        init0 =  np.random.random() *0.069
+        init1 = np.random.random() * 22.43
+        init2 = np.random.random() * -2.807
+
         m = Minuit(lnprob, throw_nan = False, pedantic = False, print_level = 0,
                   p0 = init0, p1 = init1, p2 = init2,
                   error_p0 = 1e-2, error_p1 = 1e-1, error_p2 = 1e-1, 
@@ -412,36 +574,71 @@ def y_bestFitGP(x,y,xerr,yerr, likelihoodTrial, kernelType="bkg"):
         print(kernel.get_parameter_names())
 
     elif kernelType =="sig":
-        lnProb = logLike_gp_fitgpsig(x, y, xerr)
+        lnProb = logLike_minuit_sig(x, y, xerr)
+        #lnProb = logLike_gp_fitgpsig(x, y, xerr)
         #lnProb = logLike_minuit(x, y, xerr)
 
 
         #print ("in run sig lnprob:", lnProb)
-        #print(lnProb(5701461179, 0.64, 5e5, 1.0, 1.0, 0.23, 0.46, 0.89,0, 500, 10000000))
+        print("signal lnprob: ", lnProb(9424207558.974304, 23.36140499375173, 212320.71588945185, 0.934086263121374, 677.3782035685553,5.835978239102488, 19.11598702209463, -2.3008859313936085))
 
-        min_likelihood, best_fit = fit_gp_fitgpsig_minuit(likelihoodTrial, lnProb)
-        #print(best_fit)
+        min_likelihood, best_fit = fit_gpSig_minuit(likelihoodTrial, lnProb, (9424207558.974304, 23.36140499375173, 212320.71588945185, 0.934086263121374, 677.3782035685553,5.835978239102488, 19.11598702209463, -2.3008859313936085))
+        print ("min likelihood:", min_likelihood)
+        print(best_fit)
         fit_pars = [best_fit[x] for x in FIT3_PARS]
         kargs = {x:y for x, y in best_fit.items() if x not in FIT3_PARS}
     #signal kernel
         kernel = get_kernel_Xtreme(**kargs)
+        #kernel = get_kernel(**kargs)
 
+    #if kernelType =="bkg":
     gp = george.GP(kernel, mean=Mean(fit_pars), fit_mean = True)
     gp.compute(x, yerr)
     mu, cov = gp.predict(y, x)
-    return mu, cov 
-def psuedoTest(pseudoTestNum, yBkgFit, fit_mean, yBkg, mu_xBkg):
+    return mu, cov , best_fit
+    #return 0.0, 0.0
+def psuedoTest(pseudoTestNum, yBkgFit, yErrBkgFit,fit_mean, yBkg, yErrBkg,mu_xBkg):
     chi2FitList=[]
     chi2GPList=[]
+
+    beforeWeightY = yErrBkg**2
+    weightY = yBkgFit/beforeWeightY
+    beforeWeightYFit=yErrBkgFit**2
+    weightFitY = yBkgFit/beforeWeightYFit
+
     for i in range(pseudoTestNum):
-        toyList_fit_mean=np.random.poisson(fit_mean)
-        toyList_muGP=np.random.poisson(mu_xBkg)
+        #toyList_fit_mean=np.random.poisson(fit_mean)
+        #toyList_muGP=np.random.poisson(mu_xBkg)
+
+        toyList_fit_mean_bw= np.random.poisson(fit_mean/weightY)
+        toyList_muGP_bw=np.random.poisson(mu_xBkg/weightFitY)
+
 #finding the chi2 for the list of toy thrown 
-        fitSignificance, chi2fit=res_significance(yBkgFit,toyList_fit_mean )
+        fitSignificance, chi2fit=res_significance(beforeWeightY,toyList_fit_mean_bw )
         chi2FitList.append(chi2fit)
-        GPSignificance, chi2GP=res_significance(yBkg, toyList_muGP)
+        GPSignificance, chi2GP=res_significance(beforeWeightYFit, toyList_muGP_bw)
         chi2GPList.append(chi2GP)
     chi2FitList=np.array(chi2FitList)
     chi2GPList=np.array(chi2GPList)
     return  chi2FitList, chi2GPList
 
+
+def runGP_SplusB(ys, xs_train, xerr_train, xs_fit, xerr_fit, hyperParams):
+    Amp, decay,  lengthscale, power, sub, p0, p1, p2, A, mass, tau = hyperParams
+    kernel_sig = get_kernel_SigOnly(A, mass, tau)
+    kernel_bkg = get_kernel(Amp, decay, lengthscale, power, sub)
+    
+
+    kernel = kernel_bkg + kernel_sig
+    gp = george.GP(kernel)
+    gp.compute(xs_train, np.sqrt(ys))
+
+    MAP_GP, cov_GP = gp.predict( ys - model_3param(xs_train, (p0, p1, p2), xerr_train), xs_fit)
+    MAP_GP = MAP_GP + model_3param(xs_fit,(p0, p1, p2), xerr_fit)
+    
+    K1 = kernel_bkg.get_value(np.atleast_2d(xs_train).T)
+    MAP_sig = np.dot(K1, gp.solver.apply_inverse(ys - model_3param(xs_train,(p0, p1, p2), xerr_train))) + model_3param( xs_train, (p0, p1, p2), xerr_train)
+    K2 = kernel_sig.get_value(np.atleast_2d(xs_train).T)
+    MAP_bkg = np.dot(K2, gp.solver.apply_inverse(ys-model_3param( xs_train, (p0, p1, p2),xerr_train)))
+    
+    return MAP_GP, MAP_sig, MAP_bkg
--- run_sig.py
+++ run_sig.py
@@ -35,83 +35,94 @@ def run_sig():
     xRaw, yRaw, xerrRaw, yerrRaw = getDataPoints(args.signal_plus_bkgndFile, '', 'MC_bkgndNSig_dijetgamma_g85_2j65_Ph100_ZPrimemRp5_gSM0p3_mulX1')
     xRawOffFit, yRawOffFit, xerrRawOffFit, yerrRawOffFit = getDataPoints(args.signalplusbkgndOffFit, '', 'basicBkgFrom4ParamFit')
 
+#Data=S+B
     #Data processing, cutting out the not desired range
-    xBkg, yBkg, xerrBkg, yerrBkg = dataCut(xMin, xMax, 0, xRaw, yRaw, xerrRaw, yerrRaw) # for GP bkgnd kernel and signal kernel 
-    xBkgFit, yBkgFit, xerrBkgFit, yerrBkgFit= dataCut(xMinFit, xMaxFit, 0, xRaw, yRaw,xerrRaw,yerrRaw) # for fit function 
-    #official fit fucntion fit
-    xFit, yFit, xerrFit, yerrFit = dataCut(xMinFit, xMaxFit, 0, xRawOffFit, yRawOffFit, xerrRawOffFit, yerrRawOffFit)
+    xSB, ySB, xerrSB, yerrSB = dataCut(xMin, xMax, 0, xRaw, yRaw, xerrRaw, yerrRaw) # for GP bkgnd kernel and signal kernel 
+    #simple fit function 
+    xSBFit, ySBFit, xerrSBFit, yerrSBFit= dataCut(xMinFit, xMaxFit, 0, xRaw, yRaw,xerrRaw,yerrRaw) # for fit function 
+    #official fit fucntion fit 
+    xSBOffFit, ySBOffFit, xerrOffFit, yerrOffFit = dataCut(xMinFit, xMaxFit, 0, xRawOffFit, yRawOffFit, xerrRawOffFit, yerrRawOffFit)
 
+    print("ySBOfffFit:", ySBOffFit);
     # calculate the min log likelihood of the official fit function
-    logLikeOff=logLike_3ffOff(xBkg, yBkg, xFit, yFit, xerrBkg)
+    logLikeOff=logLike_3ffOff(xSB, ySB, xSBOffFit, ySBOffFit, xerrSB)
 
+#Data=B
 #getting the bkgnd data points
     xRawbk, yRawbk, xerrRawbk, yerrRawbk = getDataPoints(args.bkgnd_file, 'dijetgamma_g85_2j65','Zprime_mjj_var')
     xBkgbk, yBkgbk, xerrBkgbk, yerrBkgbk = dataCut(xMin, xMax, 0, xRawbk, yRawbk, xerrRawbk, yerrRawbk) # for GP bkgnd kernel and signal kernel 
+
+#Data=S
+# to be added 
+
 # calculating the signal value 
-    if np.any(np.not_equal(xBkg,xBkgbk)):
+    if np.any(np.not_equal(xSB,xBkgbk)):
         print("warning, xBkg and xBkgbk value are different")
     
-    ySig = yBkg-yBkgbk
+    ySig = ySB-yBkgbk
     print("ySig", ySig)
 
     # make an evently spaced x
-    t = np.linspace(np.min(xBkg), np.max(xBkg), 500)
+    t = np.linspace(np.min(xSB), np.max(xSB), 500)
 
     #Drawing the raw data of just the bkgnd and Sig + bkgnd
     #with Canvas(f'RawSig') as can: # just to check and see everything is okay
-    #    can.ax.errorbar(xBkg, yBkg, yerr=yerrBkg, fmt='.g')
+    #    can.ax.errorbar(xSB, ySB, yerr=yerrSB, fmt='.g')
     #    can.ax.set_yscale('log')
 
-    #GP bkgnd: finidng the mean and covaraicne 
-    mu_xBkg, cov_xBkg, bestFitBkg=y_bestFitGP(xBkg,yBkg,xerrBkg, yerrBkg,3, kernelType="bkg")
+    # data SB. kernel: GP bkgnd: finidng the mean and covaraicne 
+    ymuGP_KernBkg_SB, covGP_KernBkg_SB, bestFitSB=y_bestFitGP(xSB,ySB,xerrSB, yerrSB,3, kernelType="bkg")
 
-    # GP Signal: finind the mean of covariance 
-    mu_xSig, cov_xSig, bestFitSig= y_bestFitGP(xBkg, yBkg, xerrBkg, yerrBkg, 100, kernelType="sig")
+    # data SB, kernel: GP Signal: finind the mean of covariance 
+    ymuGP_KernBG_SB, cov_xSig, bestFitSig= y_bestFitGP(xSB, ySB, xerrSB, yerrSB, 100, kernelType="sig")
 
     #Signal only fit
-    ySigFit= mu_xSig-mu_xBkg
+    ySigFit= ymuGP_KernBG_SB-ymuGP_KernBkg_SB
     print("ySigFit: ",ySigFit)
 
 #finding the fit y values 
-    fit_mean=y_bestFit3Params(xBkgFit, yBkgFit, xerrBkgFit, 1)
+    fit_mean=y_bestFit3Params(xSBFit, ySBFit, xerrSBFit, 1)
 
     kargs = {x:y for x, y in bestFitSig.items()}
     hyperParams=kargs.values()
     print("hyperParams: ",hyperParams)
 # Finding significance 
-    MAP_GP, MAP_sig, MAP_bkg=runGP_SplusB(yBkg, xBkg, xerrBkg,xBkgFit, xerrBkgFit, hyperParams)
+    MAP_GP, MAP_sig, MAP_bkg=runGP_SplusB(ySB, xSB, xerrSB,xSBOffFit, xerrOffFit, hyperParams)
+    print("MAP_bkg: ", MAP_bkg);
+    print("MAP_sig: ", MAP_sig);
       #  MAP_GP, MAP_bkg, MAP_sig = runGP_SplusB(ydata, xtoy, xtoyerr, xbins, xerrs, hyperParams)
 
 
 #finding signifiance 
-    GPSignificance, chi2=res_significance(yBkg, mu_xBkg)
-    fitSignificance, chi2fit=res_significance(yBkgFit, fit_mean)
-    GPSigSignificance, chi2SignalFit=res_significance(yBkg, mu_xSig)
+    GPSignificance, chi2=res_significance(ySB, ymuGP_KernBkg_SB)
+    fitSignificance, chi2fit=res_significance(ySBFit, fit_mean)
+    GPSigSignificance, chi2SignalFit=res_significance(ySB, ymuGP_KernBG_SB)
     GPSigOnlySignificance, chi2SigOnlySignificance = res_significance(ySig, ySigFit)
     GPSigOnlySignificanceMeg, chi2SigOnlySignificanceMeg = res_significance(ySig,MAP_sig )
-    FitSigFromOff, chi2FitOff = res_significance(yBkgFit,yFit)
-    print("x",xBkg)
-    print("significance: ",GPSigOnlySignificance)
+    FitSigFromOff, chi2FitOff = res_significance(ySB,ySBOffFit)
+    print("official significance: ", ySBOffFit);
+    #print("x",xSB)
+    #print("significance: ",GPSigOnlySignificance)
 
 #drawing the result
     ext = args.output_file_extension
     title="test"
     with Canvas(f'%s{ext}'%title, "Fit Function Official ", "GP bkgnd kernel", "GP signal+bkgnd kernel", 3) as can:
-        can.ax.errorbar(xBkg, yBkg, yerr=yerrBkg, fmt='.g', label="datapoints") # drawing the points
+        can.ax.errorbar(xSB, ySB, yerr=yerrSB, fmt='.g', label="datapoints") # drawing the points
         can.ax.set_yscale('log')
-        can.ax.plot(xBkgFit, fit_mean, '-r', label="fit function")
-        can.ax.plot(xFit, yFit, '-m', label="fit function official")
-        can.ax.plot(xBkg, mu_xBkg, '-g', label="GP bkgnd kernel") #drawing 
-        can.ax.plot(xBkg, mu_xSig, '-b', label="GP signal kernel") 
+        can.ax.plot(xSBFit, fit_mean, '.r', label="fit function")
+        can.ax.plot(xSBOffFit, ySBOffFit, '-m', label="fit function official")
+        can.ax.plot(xSB, ymuGP_KernBkg_SB, '-g', label="GP bkgnd kernel") #drawing 
+        can.ax.plot(xSB, ymuGP_KernBG_SB, '-b', label="GP signal kernel") 
         can.ax.legend(framealpha=0)
-        can.ratio.stem(xFit, FitSigFromOff, markerfmt='.', basefmt=' ')
-        #can.ratio.stem(xBkgFit, fitSignificance, markerfmt='.', basefmt=' ')
-        #can.ratio.stem(xBkgFit, testsig, markerfmt='.', basefmt=' ')
+        can.ratio.stem(xSBOffFit, FitSigFromOff, markerfmt='.', basefmt=' ')
+        #can.ratio.stem(xSBFit, fitSignificance, markerfmt='.', basefmt=' ')
+        #can.ratio.stem(xSBFit, testsig, markerfmt='.', basefmt=' ')
         can.ratio.set_ylabel("significance")
-        can.ratio2.stem(xBkg, GPSignificance, markerfmt='.', basefmt=' ')
+        can.ratio2.stem(xSB, GPSignificance, markerfmt='.', basefmt=' ')
         can.ratio2.set_ylabel("significance")
         can.ratio3.set_ylabel("significance")
-        can.ratio3.stem(xBkg, GPSigSignificance, markerfmt='.', basefmt=' ')
+        can.ratio3.stem(xSB, GPSigSignificance, markerfmt='.', basefmt=' ')
         can.ratio.axhline(0, linewidth=1, alpha=0.5)
         can.ratio2.axhline(0, linewidth=1, alpha=0.5)
         can.ratio3.axhline(0, linewidth=1, alpha=0.5)
@@ -121,29 +132,29 @@ def run_sig():
     ext = args.output_file_extension
     title="signalonly"
     with Canvas(f'%s{ext}'%title, "GPSig-GPBkgFit Sig", "", "", 2) as can:
-        can.ax.errorbar(xBkg, ySig, yerr=yerrBkg, fmt='.g', label="signal MC injected") # drawing the points
+        can.ax.errorbar(xSB, ySig, yerr=yerrSB, fmt='.g', label="signal MC injected") # drawing the points
         can.ax.set_ylim(0.1,10000.0)
         can.ax.set_yscale('log')
-        can.ax.plot(xBkg, ySigFit, '-r', label="GP Sig + bkg Kernel fit  - GP bkgnd kernel fit")
+        can.ax.plot(xSB, ySigFit, '-r', label="GP Sig + bkg Kernel fit  - GP bkgnd kernel fit")
         can.ax.legend(framealpha=0)
         ##
-        can.ratio.stem(xBkg, GPSigOnlySignificance, markerfmt='.', basefmt=' ')
+        can.ratio.stem(xSB, GPSigOnlySignificance, markerfmt='.', basefmt=' ')
         can.ratio.axhline(0, linewidth=1, alpha=0.5)
-        #can.ax.plot(xBkg, mu_xBkg, '-g', label="GP bkgnd kernel") #drawing 
+        #can.ax.plot(xSB, ymuGP_KernBkg_SB, '-g', label="GP bkgnd kernel") #drawing 
         can.save(title)
 
 #drawing the signal with meghan's method 
     ext = args.output_file_extension
     title="signalonlyMeghan"
-    with Canvas(f'%s{ext}'%title, "GPSig-GPBkgFit Sig", "", "", 2) as can:
-        can.ax.errorbar(xBkg, ySig, yerr=yerrBkg, fmt='.g', label="signal MC injected") # drawing the points
+    with Canvas(f'%s{ext}'%title, "GPSig-GPSBFit Sig", "", "", 2) as can:
+        can.ax.errorbar(xSB, ySig, yerr=yerrSB, fmt='.g', label="signal MC injected") # drawing the points
         can.ax.set_ylim(0.1,6000000)
         can.ax.set_yscale('log')
-        can.ax.plot(xBkg, MAP_bkg, '-r', label="GP Sig Kernel Only fit")
+        can.ax.plot(xSB, MAP_bkg, '-r', label="GP Sig Kernel Only fit")
         can.ax.legend(framealpha=0)
-        can.ratio.stem(xBkg, GPSigOnlySignificanceMeg, markerfmt='.', basefmt=' ')
+        can.ratio.stem(xSB, GPSigOnlySignificanceMeg, markerfmt='.', basefmt=' ')
         can.ratio.axhline(0, linewidth=1, alpha=0.5)
-        #can.ax.plot(xBkg, mu_xBkg, '-g', label="GP bkgnd kernel") #drawing 
+        #can.ax.plot(xSB, ymuGP_KernBkg_SB, '-g', label="GP bkgnd kernel") #drawing 
         can.save(title)
 
     print("min LL of off: ",logLikeOff)
