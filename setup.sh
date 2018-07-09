37f3dbec313f14cb13a0219b7bd84b68ccbdb8e9
Yvonne Ng <yvonne.ng@cern.ch>
2018-07-09 01:50:19 -0700

    relative path for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gp
 export PATH="/data11/home/dguest/install/lib/python/bin":$PATH
-export PYTHONPATH="/lustre/SCRATCH/atlas/ywng/WorkSpace/r21/gp-toys":$PYTHONPATH
-export PYTHONPATH="/lustre/SCRATCH/atlas/ywng/WorkSpace/r21/gp-toys/classes":$PYTHONPATH
-export PYTHONPATH="/lustre/SCRATCH/atlas/ywng/WorkSpace/r21/gp-toys/runFunction":$PYTHONPATH
+export PYTHONPATH=".":$PYTHONPATH
+export PYTHONPATH="./classes":$PYTHONPATH
+export PYTHONPATH="./runFunction":$PYTHONPATH
