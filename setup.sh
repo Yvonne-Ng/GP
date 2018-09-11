7dba2626fd417d3b6e432160ed49f09980b59d1e
Yvonne Ng <ywng@uci.edu>
2018-09-11 15:29:03 +0200

    setup.sh type

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ gp
 export PATH="/data11/home/dguest/install/lib/python/bin":$PATH
 export PYTHONPATH=".":$PYTHONPATH
 export PYTHONPATH="./classes":$PYTHONPATH
-export PYTHONPATH="./runFunction":$PYTHONPATH
+export PYTHONPATH="./runFunctions":$PYTHONPATH
